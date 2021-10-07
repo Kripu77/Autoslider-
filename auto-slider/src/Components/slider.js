1142ada363ddbc07bedcc403b6c8d46fd4449bd9
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-07 12:36:45 +1100

    Redo state

--- auto-slider/src/Components/slider.js
+++ auto-slider/src/Components/slider.js
@@ -3,61 +3,54 @@ import data from "./data"
 import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
 import {FaQuoteRight} from 'react-icons/fa'
 
-const Slider = () => {
-const [people, setPeople] = useState(data);
-const [ index, setIndex] = useState(0);
+const Slider = ()=>{
+const[people, setPeople] = useState(data);
+const[ index, setIndex] = useState(1);
 
-//to check the index size
-const checkFn = (index) =>{
-    if(index>people.length-1){
-        return 0
-    }
-    if(index<0){
-   return people.length-1
-    }
-    return index
-}
 
 useEffect(()=>{
-let cleanUp=setInterval(()=>{
-    setIndex((prevIndex)=>
-    checkFn(prevIndex+1))
-}, 4000)
-return ()=> clearInterval(cleanUp);
-},[])
+if(index>people.length-1){
+    setIndex(0);
 
-    return (
-      <section className='section'>
-<div className="title">
-    <h2>
-        <span> /</span>
-Inspirational Quotes
-    </h2>
-</div>
+}
+if(index<0){
+    setIndex(people.length-1)
+}
+
+}, [index]) //useEffect will only trrigger if the value of index changes
+
+//for autoslider we will use setInterval function
+
+    return (<section className="section">
+        <h2 className="title"> <span>//</span>  Inspirational Quotes</h2>
 <div className="section-center">
     {people.map((person, personIndex)=>{
-        const{id, image, name, text, quote} = person;
-let position = 'nextSlide'
-if(personIndex === index){
-    position= 'activeSlide';
-}
-if(personIndex===index-1 || (index==0 && personIndex === people.length-1)){
-    position= 'lastSlide';
-}
-
-        return <article key= {id} className={position}>
-            <img src={image} alt={name}  className="person-img"/>
-            <h4> {name}</h4>
-            <p className="title">{text}</p>
-            <p className="text"> {quote}</p>
+        const{id, name, image, text,  job} = person;
+let position = "nextSlide";
+        if(personIndex === index){
+position = "activeSlide"
+        }
+        if(personIndex=== index-1 || index === 0 && personIndex === people.length-1){
+            position= "lastSlide";
+        }
+        return <article key={id} className={position}>
+            <img src={image} alt={name} className="person-img"/>
+            <h4 className="name"> {name}</h4>
+                <h4 className="name"> {job}</h4>
+            <p className="title"> {text}</p>
+        
             <FaQuoteRight className="icon"/>
-        </article>
+       
+
+        </article> 
+
     })}
-    <button className="prev" onClick={()=>setIndex((()=>checkFn(index-1)))}> <FiChevronLeft/></button>
-       <button className="next" onClick={()=>{setIndex((()=>checkFn(index+1)))}}> <FiChevronRight/></button>
+<button  className="prev" onClick={()=>setIndex(index-1)}> <FiChevronLeft/></button>
+<button  className="next"onClick={()=>setIndex(index+1)}> <FiChevronRight/></button>
 </div>
-      </section>
-    )
+    </section>)
+
+
 }
 
-export default Slider
+export default Slider;
